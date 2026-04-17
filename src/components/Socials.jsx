c3c99336acf8c27f6039e85c5958c8fa33bed341
Andreas <andreasjonsson00@gmail.com>
2026-04-17 19:38:55 +0200

    new icons for social links, remove unused assets

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import githubImg from "../assets/github-mark.svg";
-import linkedinImg from "../assets/linkedin-mark.svg";
+import githubImg from "../assets/github-mark.png";
+import linkedinImg from "../assets/linkedin-mark.png";
 import emailImg from "../assets/email-mark.png";
 
 const Socials = () => {
@@ -11,17 +11,9 @@ const Socials = () => {
         href="https://github.com/Andreasjonsson00"
         target="_blank"
       >
-        <img className="github-icon w-6" src={githubImg} alt="GitHub Icon" />
-      </a>
-      <a
-        className="flex flex-col items-center transition duration-300 ease-out hover:scale-[1.3] hover:opacity-80"
-        href="mailto:andreasjonsson00@gmail.com"
-      >
-        <img
-          className="email-icon w-6 mt-0.5"
-          src={emailImg}
-          alt="Email Icon"
-        />
+        <img className="github-icon w-6 mt-0.5" 
+        src={githubImg} 
+        alt="GitHub Icon" />
       </a>
       <a
         className="flex flex-col items-center transition duration-300 ease-out hover:scale-[1.3] hover:opacity-80"
@@ -29,10 +21,18 @@ const Socials = () => {
         target="_blank"
       >
         <img
-          className="linkedin-icon w-6"
+          className="linkedin-icon w-6 mt-1"
           src={linkedinImg}
           alt="LinkedIn Icon"
         />
+      </a>
+      <a
+        className="flex flex-col items-center transition duration-300 ease-out hover:scale-[1.3] hover:opacity-80"
+        href="mailto:andreasjonsson00@gmail.com"
+      >
+        <img className="email-icon w-6 mt-1.5" 
+        src={emailImg} 
+        alt="Email Icon" />
       </a>
     </nav>
   );
