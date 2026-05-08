fcc61d76f734579cffc31a4b8c7d541fd3e1eb92
Andreas <andreasjonsson00@gmail.com>
2026-05-08 13:17:29 +0200

    put tech and link to bottom of cards

--- src/components/ProjectsCard.jsx
+++ src/components/ProjectsCard.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 const ProjectsCard = ({ title, description, tech, link }) => {
   return (
-    <div className="bg-[#1a1a1a] p-6 rounded-xl shadow-md hover:scale-105 transition-transform duration-300">
+    <div className="bg-[#1a1a1a] p-6 rounded-xl shadow-md hover:scale-105 transition-transform duration-300 flex h-full flex-col">
       <h3 className="text-xl font-semibold text-white mb-2">{title}</h3>
 
       <p className="text-gray-400 mb-4">{description}</p>
 
-      <div className="flex flex-col items-center justify-between mt-4">
+      <div className="mt-auto flex flex-col items-center gap-2 pt-4">
         <p className="text-sm text-purple-700">{tech}</p>
         <a
           href={link}
