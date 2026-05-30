447690bad79f1b1474290171b0024ef591806ad7
Andreas <andreasjonsson00@gmail.com>
2026-05-30 20:34:12 +0200

    remove project

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -9,13 +9,13 @@ const projects = [
     tech: "React • API • Tailwind",
     link: "https://pokemon-api-app-two.vercel.app/",
   },
-  {
-    title: "Receptsamlingen",
-    description:
-    "Create and add your favorite recipes. View detailed information about the recipes and share them with your friends.",
-    tech: "React • Node.js • Express • PostgreSQL",
-    link: "https://receptsamlingen-app.vercel.app",
-  },
+  // {
+  //   title: "Receptsamlingen",
+  //   description:
+  //   "Create and add your favorite recipes. View detailed information about the recipes and share them with your friends.",
+  //   tech: "React • Node.js • Express • PostgreSQL",
+  //   link: "https://receptsamlingen-app.vercel.app",
+  // },
   {
     title: "Movie Database",
     description:
